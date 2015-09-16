f6bbc130a8f797253094a669af020f6571d9d834
Lawrence23 <lawrence@qburst.com>
2015-09-16 13:24:41 +0530

    first commit

---
+++ master/js/modules/lazyload/lazyload.constants.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.lazyload')
+        .constant('APP_REQUIRES', {
+          // jQuery based and standalone scripts
+          scripts: {
+            'modernizr':          ['vendor/modernizr/modernizr.js'],
+            'icons':              ['vendor/fontawesome/css/font-awesome.min.css',
+                                   'vendor/simple-line-icons/css/simple-line-icons.css']
+          },
+          // Angular based script (use the right module name)
+          modules: [
+            {name: 'datatables', files: ['vendor/datatables/media/css/jquery.dataTables.css',
+                                        'vendor/datatables/media/js/jquery.dataTables.js',
+                                        'vendor/angular-datatables/dist/angular-datatables.js'], serie: true}
+            // {name: 'toaster', files: ['vendor/angularjs-toaster/toaster.js', 'vendor/angularjs-toaster/toaster.css']}
+          ]
+        })
+        ;
+
+})();
